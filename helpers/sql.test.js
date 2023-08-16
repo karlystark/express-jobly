a85527c9bf533dd2ce6a6f83c23410990c5899d5
Karly Stark <karlystark@gmail.com>
2023-08-16 16:33:37 -0700

    helper function getFilteredQuery working and tested

--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -2,7 +2,7 @@
 
 const { BadRequestError } = require("../expressError");
 
-const { sqlForPartialUpdate } = require("./sql");
+const { sqlForPartialUpdate, getFilteredQuery } = require("./sql");
 
 
 describe("sqlForPartialUpdate", function () {
@@ -50,4 +50,64 @@ describe("sqlForPartialUpdate", function () {
 });
 
 
-describe("sql")
+describe("getFilteredQuery", function () {
+  test("returns expected results when passed all three queries in queryData",
+    function () {
+      const result = getFilteredQuery({
+        nameLike: "dev",
+        minEmployees: "100",
+        maxEmployees: "5000"
+      });
+
+      expect(result).toEqual({
+        where: "name ILIKE %$1% AND num_employees <= $2 AND num_employees >= $3",
+        values: ['dev', 100, 5000]
+      });
+    });
+
+
+  test("returns expected results when passed two queries in queryData",
+  function(){
+    const result = getFilteredQuery({
+      nameLike: "dev",
+      maxEmployees: "5000"
+    });
+
+    expect(result).toEqual({
+      where: "name ILIKE %$1% AND num_employees >= $2",
+      values: ['dev', 5000]
+    });
+  });
+
+  test("returns expected results when passed one query in queryData",
+  function(){
+    const result = getFilteredQuery({
+      maxEmployees: "5000"
+    });
+
+    expect(result).toEqual({
+      where: "num_employees >= $1",
+      values: [5000]
+    });
+  });
+
+  test("returns BadRequestError when no data passed", function(){
+    try {
+    const result = getFilteredQuery({});
+    } catch (err){
+      expect(err instanceof BadRequestError).toBeTruthy();
+    }});
+
+  test("return BadRequestError when minEmployees is greater than maxEmployees",
+  function(){
+    try {
+      const result = getFilteredQuery({
+        minEmployees: "5000",
+        maxEmployees: "100"
+      });
+    } catch(err){
+      expect(err instanceof BadRequestError).toBeTruthy();
+    }
+  });
+
+});
