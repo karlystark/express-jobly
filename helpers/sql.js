9c77e506c05163d1bc927d46bc70c24fb352fd7e
Karly Stark <karlystark@gmail.com>
2023-08-16 11:31:36 -0700

    WIP docstring for sqlForPartialUpdate

--- helpers/sql.js
+++ helpers/sql.js
@@ -2,7 +2,21 @@
 
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/**
+ * Receives an object of column names and update data and a dictionary of JS/SQL variable names
+ * Takes Javascript keys from dataToUpdate and maps their SQL column names to sanitized values
+ * Returns an object containing two keys - setCols is a string of column names and their sanitized values,
+ * Values is an array of updated column values/data
+ *
+ *
+ *
+ *
+ * @param {*} dataToUpdate  -- object that can contain any number of data keys/values
+ * @param {*} jsToSql -- object keys are javascript variable names, values are sql columns
+ * @returns an object containing two keys - setCols is a string of column names and their sanitized values
+ * values is an array of updated column values/data
+ */
+
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
@@ -12,6 +26,8 @@ function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const cols = keys.map((colName, idx) =>
       `"${jsToSql[colName] || colName}"=$${idx + 1}`,
   );
+  console.log("cols=", cols);
+  console.log("join looks like=", cols.join(", "));
 
   return {
     setCols: cols.join(", "),
